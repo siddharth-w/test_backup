aaa6c07aa37dc0bee2e6a19141cba3ae85a79e1c
siddharth-w <68491572+siddharth-w@users.noreply.github.com>
2024-08-30 23:44:32 +0530

    Update move_joint.py

--- move_joint.py
+++ move_joint.py
@@ -1,37 +1,56 @@
-from owl_client import OwlClient, Joint
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Pose
+from std_msgs.msg import Float64MultiArray
+from owl_client import OwlClient
+import owl_client
 import time
 
-client = OwlClient("10.42.0.3")
-jointSpeed = 1 #degrees/sec
+class MoveJointNode(Node):
+    def __init__(self):
+        super().__init__('move_joint_node')
 
-# Wait for robot to be available
-while not client.is_running():
-    time.sleep(0.2)
+        self._robot_ip = "10.42.0.3"
+        self.client = OwlClient(self._robot_ip)
 
-#create joint goals for robot
+        self.jointSpeed = 10 #degree/sec
+        
+        while not self.client.is_running():
+            time.sleep(0.2)
+            self.get_logger().info("Waiting for robot to be available...")
 
-#zero configuration
-zero_position = Joint()
-zero_position.Base = 0.0
-zero_position.Shoulder = 0.0
-zero_position.Elbow  = 0.0
-zero_position.Wrist1 = 0.0
-zero_position.Wrist2 = 0.0
-zero_position.Wrist3 = 0.0
+        self.subscription = self.create_subscription(
+            Float64MultiArray,
+            'robot/joint_state_update',
+            self.movee_callback,
+            1
+        )
 
-#valid configuration
-valid_position = Joint()
-valid_position.Base = 0.2
-valid_position.Shoulder = 0.0
-valid_position.Elbow  = 0.0  #(-90)
-valid_position.Wrist1 = 0.0
-valid_position.Wrist2 = 0.0
-valid_position.Wrist3 = 0.0
+    def move_callback(self, msg):
 
-count = 0
-while count < 1 :
-    client.move_to_joint(zero_position, jointSpeed)
-    time.sleep(5)
-    client.move_to_joint(valid_position, jointSpeed)
-    time.sleep(1)
-    count += 1
+        
+        #zero configuration
+        zero_position = Joint()
+        zero_position.Base = msg.data[0]
+        zero_position.Shoulder = msg.data[1]
+        zero_position.Elbow  = msg.data[1]
+        zero_position.Wrist1 = msg.data[2]
+        zero_position.Wrist2 = msg.data[3]
+        zero_position.Wrist3 = msg.data[4]
+
+        self.get_logger().info(f'Moving to pose: {zero_position}')
+        client.move_to_joint(zero_position, self.jointSpeed)
+        time.sleep(5)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MOveJointNode()
+    rclpy.spin(node)
+    # node.destroy_node()
+    # rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+
+
